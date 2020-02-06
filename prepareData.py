d8ef03e413014b6c3b92d89112908b3cf5666bd2
a.yankovich <EndrIIMail@gmail.com>
2020-02-06 09:48:00 +0300

    fix repo script

--- prepareData.py
+++ prepareData.py
@@ -1,13 +1,9 @@
 import os
-from pathlib import Path
 import sys
 import shutil   
 
 location = sys.argv[1]
 
 PWD = os.path.dirname(os.path.realpath(__file__))
-
-shutil.rmtree(location)
-Path(location).mkdir(parents=True, exist_ok=True)
 
 shutil.copytree(PWD + "/packages", location)
